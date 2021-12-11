422dffd726de3fbaa5d72b2df73f37e6cf3b4343
Juan Campos <juanlad1221@gmail.com>
2021-12-11 13:06:37 -0300

    cambios modelos

---
+++ info_blog/comments/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.db.models.fields import EmailField
+import post
+from post.models import Post
+
+# Create your models here.
+class Comments(models.Model):
+    comment = models.CharField(max_length=500)
+    email = models.EmailField(max_length=200)
+    created = models.DateTimeField()
+    active = models.BooleanField(default=True)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.comment
--- info_blog/post/models.py
+++ info_blog/post/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Category(models.Model):
+    category = models.CharField(max_length=50)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.category
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=500)
+    created = models.DateTimeField()
+    active = models.BooleanField(default=True)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
